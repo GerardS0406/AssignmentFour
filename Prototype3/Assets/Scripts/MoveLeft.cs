6332a499ed99ff03b70a79d2787382e9ce2f8210
GerardS0406 <48145351+GerardS0406@users.noreply.github.com>
2022-02-20 18:16:36 -0600

    Week 1 Prototype 3

---
+++ Prototype3/Assets/Scripts/MoveLeft.cs
@@ -0,0 +1,34 @@
+﻿/*
+* Gerard Lamoureux
+* Prototype 3
+* Handles moving objects left
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+    private float speed = 30f;
+    private float leftBound = -4;
+
+    private PlayerController playerControllerScript;
+
+    void Start()
+    {
+        playerControllerScript = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerControllerScript.gameOver == false)
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+
+        if(transform.position.x < leftBound && gameObject.CompareTag("Obstacle"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Prototype3/Assets/Scripts/RepeatBackground.cs
@@ -0,0 +1,31 @@
+﻿/*
+* Gerard Lamoureux
+* Prototype 3
+* Handles repeating of background
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatBackground : MonoBehaviour
+{
+    private Vector3 startPosition;
+    private float repeatWidth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position;
+        repeatWidth = GetComponent<BoxCollider>().size.x / 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.x < startPosition.x - repeatWidth)
+        {
+            transform.position = startPosition;
+        }
+    }
+}
